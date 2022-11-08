28e21e0d1652c05664dc26fc8b4890fc1923ece4
xieminglin <1150383838@qq.com>
2022-11-08 13:33:32 +0800

    Initial Commit

---
+++ test.code/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test.code
+//
+//  Created by 谢明霖 on 2022/11/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test.code/test_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_codeApp.swift
+//  test.code
+//
+//  Created by 谢明霖 on 2022/11/8.
+//
+
+import SwiftUI
+
+@main
+struct test_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
